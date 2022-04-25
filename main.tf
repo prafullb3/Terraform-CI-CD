4e623e9c3fdfb9df12b541f465e50681d5c40444
prafullb3 <42909468+prafullb3@users.noreply.github.com>
2022-04-25 14:19:47 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
